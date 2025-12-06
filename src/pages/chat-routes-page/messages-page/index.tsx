06b05ac527f58185ce995c736ad8662096dcfb86
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2025-12-07 00:26:15 +0700

    update

--- src/pages/chat-routes-page/messages-page/index.tsx
+++ src/pages/chat-routes-page/messages-page/index.tsx
@@ -381,7 +381,7 @@ export default function MessagesPage() {
   // =======================================================================
 
   return (
-    <div className="flex h-screen bg-gray-900">
+    <div className="flex h-screen bg-gray-50">
       <div className="w-80 bg-white border-r border-gray-200 flex flex-col">
         <SharedTopBar
           onFriendAdded={refetchRooms}
