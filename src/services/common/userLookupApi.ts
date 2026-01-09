b758e5958e54042204bb8ac892449896e8f34583
phatdang <phatdang19052004@gmail.com>
2026-01-09 14:36:29 +0700

    feat(service.user): thêm userApi chứa api liên quan tới user

--- src/services/common/userLookupApi.ts
+++ src/services/common/userLookupApi.ts
@@ -1,9 +1,5 @@
 import axiosClient from "@/lib/axiosClient.ts";
-import type {
-  ApiResponse,
-  PageResponse,
-  PaginationParams,
-} from "@/types/common/apiResponse";
+import type { ApiResponse } from "@/types/common/apiResponse";
 import type { UserSummaryResponse } from "@/types/common/userSummary";
 import type { UserSummarySimpleResponse } from "@/types/common/userSummarySimpleResponse";
 
@@ -16,18 +12,5 @@ export const lookupApi = (email: string) => {
 export const lookupByIdApi = (id: number) => {
   return axiosClient.get<ApiResponse<UserSummarySimpleResponse>>(
     `/users/lookup/id?id=${id}`
-  );
-};
-
-export const getAllUsers = (params: PaginationParams) => {
-  return axiosClient.get<ApiResponse<PageResponse<UserSummaryResponse>>>(
-    "/users",
-    {
-      params: {
-        page: params.page,
-        size: params.size,
-        sort: params.filter || "id,desc",
-      },
-    }
   );
 };
