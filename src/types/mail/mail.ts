2666cbbbfa4f3bf0ee220531e4dbd57753d77a3d
phatdang <phatdang19052004@gmail.com>
2026-02-09 16:53:02 +0700

    refactor: add ACCOUNT_ACTIVATION to otpType in SendOtpRequest and VerifyOtpRequest

--- src/types/mail/mail.ts
+++ src/types/mail/mail.ts
@@ -1,6 +1,6 @@
 export interface SendOtpRequest {
   email: string;
-  otpType: "USER_FORGET_PASSWORD" | "ADMIN_VERIFICATION";
+  otpType: "USER_FORGET_PASSWORD" | "ADMIN_VERIFICATION" | "ACCOUNT_ACTIVATION";
 }
 
 export interface SendOtpResponse {
@@ -12,7 +12,7 @@ export interface SendOtpResponse {
 export interface VerifyOtpRequest {
   otp: string;
   mailRecipient: string;
-  otpType: "USER_FORGET_PASSWORD" | "ADMIN_VERIFICATION";
+  otpType: "USER_FORGET_PASSWORD" | "ADMIN_VERIFICATION" | "ACCOUNT_ACTIVATION";
 }
 
 export interface VerifyOtpResponse {
